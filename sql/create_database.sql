5b5ee0fbf34dcdb75c063265986cc636ae444ffd
fanjingwenvi <fanjingwenvi@qq.com>
2022-06-28 22:40:54 +0200

    add sql folder

---
+++ sql/create_database.sql
@@ -0,0 +1,13 @@
+create database taobao;
+use taobao;
+## set taobao as default database 
+create table user_behavior(user_id int(9), item_id int(9), 
+	category_id int(9), behavior_type varchar(5), timestamp int(14))
+## char vs varchar: varchar is more flexible for the space 
+## timestamp vs int: 
+## date, datetime, timestamp, time, year,
+
+## tables 
+## view: combine mutiple tables to create a view for report 
+## stored procedures: query the data 
+## fuctions 
